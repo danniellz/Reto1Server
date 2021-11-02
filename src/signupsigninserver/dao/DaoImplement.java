e2fc18e88e1b9129a19fb27cc71732edc463c0a0
mikelmatilla <mikel.matilla1@gmail.com>
2021-11-02 12:08:05 +0100

    Worker terminado

---
+++ src/signupsigninserver/dao/DaoImplement.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package signupsigninserver.dao;
+
+import message.Message;
+import signable.Signable;
+import user.User;
+
+/**
+ *
+ * @author 2dam
+ */
+public class DaoImplement implements Signable {
+
+    public DaoImplement() {
+    }
+
+    @Override
+    public Message signIn(User user) {
+        return null;
+        
+    }
+
+    @Override
+    public Message signUp(User user) {
+        return null;
+        
+    }
+    
+}
--- src/signupsigninserver/worker/Worker.java
+++ src/signupsigninserver/worker/Worker.java
@@ -8,46 +8,53 @@ package signupsigninserver.worker;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import signupsigninserver.dao.DaoFactory;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import message.Message;
+import signable.Signable;
 
 /**
- *
- * @author 2dam
+ * @author Mikel Matilla
  */
 public class Worker extends Thread {
+    
+    private static final Logger LOG = Logger.getLogger(Worker.class.getName());
     
     protected Socket socket = null;
     protected Message message   = null;
     
-    public Worker (Socket socket, Message message) {
+    public Worker (Socket socket) {
         this.socket = socket;
-        this.message = message;
     }
     
     public void run () {
         try {
-        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-        message = (Message) ois.readObject();
-        //DUDA
-        switch (message.getAccion()) {
-            case SIGNUP:
-                break;
-            case SIGNIN:
-                break;
-            case LOGOUT:
-                break;
-            default:
-                break;
-        }
-        
-        oos.close();
-        ois.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            LOG.info("Sending info to DaoImplement");
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            message = (Message) ois.readObject();
+            
+            Signable sign = new DaoFactory().getDao();
+            switch (message.getAccion()) {
+                case SIGNUP:
+                    oos.writeObject(sign.signUp(message.getUser()));
+                    LOG.info("SignUp");
+                    break;
+                case SIGNIN:
+                    oos.writeObject(sign.signIn(message.getUser()));
+                    LOG.info("SignIn");
+                    break;
+                default:
+                    LOG.severe("Unknown error");
+                    break;
+            }
+
+            oos.close();
+            ois.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
         }
